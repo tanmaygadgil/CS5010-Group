49644997086e819d3e70fc8b370c672872597b0b
Sunny Lee <lees19@wit.edu>
2023-03-08 21:22:03 -0500

    load and save method

--- Assignment4/src/Model/Model.java
+++ Assignment4/src/Model/Model.java
@@ -1,3 +1,14 @@
+package Model;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface Model {
+
+  void load(String imageName, String destImage);
+
+  void save(String filePath, String imageName) throws IOException;
+
+
 
 }
