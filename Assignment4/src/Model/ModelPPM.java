ece68bd938cff52d92ae6fafe113fea848fee19f
Sunny Lee <lees19@wit.edu>
2023-03-11 00:16:11 -0500

    flip and increment

--- Assignment4/src/Model/ModelPPM.java
+++ Assignment4/src/Model/ModelPPM.java
@@ -10,9 +10,10 @@ public class ModelPPM implements Model {
 
   private final HashMap<String, int[][][]> imageMap;
 
-  public ModelPPM(){
+  public ModelPPM() {
     this.imageMap = new HashMap<>();
   }
+
   @Override
   public void load(String filePath, String destImage) throws FileNotFoundException {
     //size = (3, 768, 1024)
@@ -42,13 +43,57 @@ public class ModelPPM implements Model {
   }
 
   @Override
-  public void brighten(int Increment, String imageName, String destImage) {
+  public void brighten(int increment, String imageName, String destImage) {
+    int[][][] image = imageMap.get(imageName);
+    for(int i = 0; i < image.length; i++) {
+      for(int j = 0; j < image[0].length; j++) {
+        for(int k = 0; k < image[0][0].length; k++) {
+          image[i][j][k] = image[i][j][k] + increment;
+        }
+      }
+    }
 
+    imageMap.put(destImage, image);
   }
 
   @Override
   public void flip(Axes axis, String imageName, String destImage) {
+    int[][][] image = this.imageMap.get(imageName);
 
+    if (axis == Axes.HORIZONTAL) {
+      for(int i = 0; i < image.length; i++) {
+        image[i] = flipHorizontal(image[i]);
+      }
+      imageMap.put(destImage, image);
+    } else if (axis == Axes.VERTICAL) {
+      for (int i = 0; i < image.length; i++) {
+        image[i] = flipVertical(image[i]);
+      }
+      imageMap.put(destImage, image);
+    } //else throw error?
+  }
+
+  private int[][] flipVertical(int[][] image) {
+    int[] temp = new int[image.length];
+    for (int i = 0; i < image.length / 2; i++) {
+      temp = image[image.length - i - 1];
+      image[image.length - i - 1] = image[i];
+      image[i] = temp;
+    }
+
+    return image;
+  }
+
+  private int[][] flipHorizontal(int[][] image) {
+    for (int i = 0; i < image.length; i++) { //rows
+      for (int j = 0; j < image[0].length / 2; j++) { //cols
+        int temp = image[i][(image[0].length) - j - 1];
+        image[i][image[0].length - j - 1] = image[i][j];
+        image[i][j] = temp;
+      }
+    }
+
+    return image;
   }
 
   @Override
