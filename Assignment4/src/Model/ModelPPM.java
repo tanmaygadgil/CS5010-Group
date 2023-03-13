52870b637f95cefb9bf21ca3505aea1182783e91
Sunny Lee <lees19@wit.edu>
2023-03-13 01:58:41 -0400

    test for flip, save

--- Assignment4/src/Model/ModelPPM.java
+++ Assignment4/src/Model/ModelPPM.java
@@ -9,6 +9,8 @@ import java.util.HashMap;
 public class ModelPPM implements Model {
 
   private final HashMap<String, int[][][]> imageMap;
+  private int width;
+  private int height;
 
   public ModelPPM() {
     this.imageMap = new HashMap<>();
@@ -18,6 +20,8 @@ public class ModelPPM implements Model {
   public void load(String filePath, String destImage) throws FileNotFoundException {
     //size = (3, 768, 1024)
     int[][][] image = ImageUtil.readPPM(filePath);
+    this.height = image[0].length;
+    this.width = image[0][0].length;
     this.imageMap.put(destImage, image);
   }
 
@@ -26,17 +30,15 @@ public class ModelPPM implements Model {
     FileOutputStream fout = new FileOutputStream(filePath);
 
     int[][][] image = this.imageMap.get(imageName);
-    int width = image[0][0].length;
-    int height = image[0].length;
 
     fout.write("P3\n".getBytes());
     fout.write(String.format("%d %d\n255\n", width, height).getBytes());
 
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        fout.write((new Integer(image[0][i][j]).toString() + "\n").getBytes());
-        fout.write((new Integer(image[1][i][j]).toString() + "\n").getBytes());
-        fout.write((new Integer(image[2][i][j]).toString() + "\n").getBytes());
+    for (int i = 0; i < height; i++) { //rows
+      for (int j = 0; j < width; j++) { //cols
+        for(int k = 0; k < image.length; k++) {
+          fout.write((new Integer(image[k][i][j]).toString() + "\n").getBytes());
+        }
       }
     }
 
@@ -45,9 +47,9 @@ public class ModelPPM implements Model {
   @Override
   public void brighten(int increment, String imageName, String destImage) {
     int[][][] image = imageMap.get(imageName);
-    for(int i = 0; i < image.length; i++) {
-      for(int j = 0; j < image[0].length; j++) {
-        for(int k = 0; k < image[0][0].length; k++) {
+    for (int i = 0; i < image.length; i++) {
+      for (int j = 0; j < height; j++) {
+        for (int k = 0; k < width; k++) {
           image[i][j][k] = image[i][j][k] + increment;
         }
       }
@@ -61,7 +63,7 @@ public class ModelPPM implements Model {
     int[][][] image = this.imageMap.get(imageName);
 
     if (axis == Axes.HORIZONTAL) {
-      for(int i = 0; i < image.length; i++) {
+      for (int i = 0; i < image.length; i++) {
         image[i] = flipHorizontal(image[i]);
       }
       imageMap.put(destImage, image);
@@ -75,9 +77,9 @@ public class ModelPPM implements Model {
 
   private int[][] flipVertical(int[][] image) {
     int[] temp = new int[image.length];
-    for (int i = 0; i < image.length / 2; i++) {
-      temp = image[image.length - i - 1];
-      image[image.length - i - 1] = image[i];
+    for (int i = 0; i < height / 2; i++) {
+      temp = image[height - i - 1];
+      image[height - i - 1] = image[i];
       image[i] = temp;
     }
 
@@ -85,10 +87,10 @@ public class ModelPPM implements Model {
   }
 
   private int[][] flipHorizontal(int[][] image) {
-    for (int i = 0; i < image.length; i++) { //rows
-      for (int j = 0; j < image[0].length / 2; j++) { //cols
-        int temp = image[i][(image[0].length) - j - 1];
-        image[i][image[0].length - j - 1] = image[i][j];
+    for (int i = 0; i < height; i++) { //rows
+      for (int j = 0; j < width / 2; j++) { //cols
+        int temp = image[i][width - j - 1];
+        image[i][width - j - 1] = image[i][j];
         image[i][j] = temp;
       }
     }
@@ -98,20 +100,113 @@ public class ModelPPM implements Model {
 
   @Override
   public void greyscale(ImageComponents RGB, String imageName, String destImage) {
+    int[][][] greyscale = new int[1][height][width];
 
+    switch (RGB) {
+      case RED:
+        greyscale = getRed(imageName);
+        break;
+      case GREEN:
+        greyscale = getGreen(imageName);
+        break;
+      case BLUE:
+        greyscale = getBlue(imageName);
+        break;
+      case LUMA:
+        greyscale = greyscaleLuma(imageName);
+        break;
+      case VALUE:
+        greyscale = greyscaleValue(imageName);
+        break;
+      case INTENSITY:
+        greyscale = greyscaleIntensity(imageName);
+        break;
+    }
+
+    imageMap.put(destImage, greyscale);
   }
 
+  private int[][][] getRed(String imageName) {
+    int[][][] image = imageMap.get(imageName);
+    int[][][] grey = new int[1][height][width];
+    grey[0] = image[0];
 
-  @Override
-  public void rgbSplit(String DestImage, String DestImageRed, String DestImageGreen,
-      String DestImageBlue) {
+    return grey;
+  }
 
+  private int[][][] getGreen(String imageName) {
+    int[][][] image = imageMap.get(imageName);
+    int[][][] grey = new int[1][height][width];
+    grey[0] = image[1];
+
+    return grey;
+  }
+
+  private int[][][] getBlue(String imageName) {
+    int[][][] image = imageMap.get(imageName);
+    int[][][] grey = new int[1][height][width];
+    grey[0] = image[2];
+
+    return grey;
+  }
+
+  private int[][][] greyscaleValue(String imageName) {
+    int[][][] image = imageMap.get(imageName);
+    int[][][] grey = new int[1][height][width];
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        grey[0][i][j] = Math.max(Math.max(image[0][i][j], image[1][i][j]), image[2][i][j]);
+      }
+    }
+
+    return grey;
+  }
+
+  private int[][][] greyscaleIntensity(String imageName) {
+    int[][][] image = imageMap.get(imageName);
+    int[][][] grey = new int[1][height][width];
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        grey[0][i][j] = (image[0][i][j] + image[1][i][j] + image[2][i][j]) / image.length;
+      }
+    }
+
+    return grey;
+  }
+
+  private int[][][] greyscaleLuma(String imageName) {
+    int[][][] image = imageMap.get(imageName);
+    int[][][] grey = new int[1][height][width];
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        grey[0][i][j] = (int) (0.2126 * image[0][i][j] + 0.7152 * image[1][i][j]
+            + 0.0722 * image[2][i][j]);
+      }
+    }
+
+    return grey;
   }
 
   @Override
-  public void rgbCombine(String DestImage, String DestImageRed, String DestImageGreen,
-      String DestImageBlue) {
+  public void rgbSplit(String imageName, String destImageRed, String destImageGreen,
+      String destImageBlue) {
+      imageMap.put(destImageRed, getRed(imageName));
+      imageMap.put(destImageGreen, getGreen(imageName));
+      imageMap.put(destImageBlue, getBlue(imageName));
+  }
 
+  @Override
+  public void rgbCombine(String destImage, String destImageRed, String destImageGreen,
+      String destImageBlue) {
+    int[][][] image = new int[3][height][width];
+    image[0] = imageMap.get(destImageRed)[0];
+    image[1] = imageMap.get(destImageGreen)[0];
+    image[2] = imageMap.get(destImageBlue)[0];
+
+    imageMap.put(destImage, image);
   }
 
 }
---
+++ Assignment4/test/Model/ModelTest.java
@@ -0,0 +1,104 @@
+package Model;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModelTest {
+
+  Model model;
+
+  @Before
+  public void setUp() {
+    this.model = new ModelPPM();
+  }
+
+  @Test
+  public void testSave() throws IOException {
+    Model model = new ModelPPM();
+
+    model.load("test/Model/testImage.ppm", "testImage");
+    model.save("test/Model/testSaveImage.ppm", "testImage");
+    int[][][] imageSaved = ImageUtil.readPPM("test/Model/testSaveImage.ppm");
+    int[][][] imageOriginal = ImageUtil.readPPM("test/Model/testImage.ppm");
+
+    assertEquals(imageOriginal.length, imageSaved.length);
+    assertEquals(imageOriginal[0].length, imageSaved[0].length);
+    assertEquals(imageOriginal[0][0].length, imageSaved[0][0].length);
+
+    for (int i = 0; i < imageSaved.length; i++) {
+      for (int j = 0; j < imageSaved[0].length; j++) {
+        for (int k = 0; k < imageSaved[0][0].length; k++) {
+          assertEquals(imageOriginal[i][j][k], imageSaved[i][j][k]);
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testBrighten() throws IOException {
+    String path = "test/Model/testImage.ppm";
+    int[][][] image = ImageUtil.readPPM(path);
+    int increment = 10;
+
+    model.load(path, "testImage");
+    model.brighten(increment, "testImage", "testImageBrighten");
+    model.save("test/Model/testImageBrighten.ppm", "testImageBrighten");
+    int[][][] imageBrighten = ImageUtil.readPPM("test/Model/testImageBrighten.ppm");
+
+    for (int i = 0; i < image.length; i++) {
+      for (int j = 0; j < image[0].length; j++) {
+        for (int k = 0; k < image[0][0].length; k++) {
+          assertEquals(imageBrighten[i][j][k] - image[i][j][k], 10);
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testFlipVertical() throws IOException {
+    String path = "test/Model/testImageAscendingRows.ppm";
+    int[][][] image = new int[3][3][4];
+    for (int i = 3; i >= 1; i--) {
+      for (int j = 3; j >= 1; j--) {
+        for (int k = 4; k >= 1; k--) {
+          image[3 - i][3 - j][4 - k] = j;
+        }
+      }
+    }
+
+    model.load(path, "testImageAscendingRows");
+    model.flip(Axes.VERTICAL, "testImageAscendingRows",
+        "testImageAscendingRowsVertFlip");
+    model.save("test/Model/testImageAscendingRowsVertFlip.ppm",
+        "testImageAscendingRowsVertFlip");
+    int[][][] imageVert = ImageUtil.readPPM("test/Model/testImageVert.ppm");
+
+    assertEquals(imageVert, image);
+  }
+
+  @Test
+  public void testFlipHorizontal() throws IOException {
+    String path = "test/Model/testImageAscendingCols.ppm";
+    int[][][] image = new int[3][3][4];
+    for (int i = 3; i >= 1; i--) {
+      for (int j = 3; j >= 1; j--) {
+        for (int k = 4; k >= 1; k--) {
+          image[3 - i][3 - j][4 - k] = k;
+        }
+      }
+    }
+
+    model.load(path, "testImageAscendingCols");
+    model.flip(Axes.HORIZONTAL, "testImageAscendingCols",
+        "testImageAscendingColsHoriFlip");
+    model.save("test/Model/testImageAscendingColsHoriFlip.ppm",
+        "testImageAscendingColsHoriFlip");
+    int[][][] imageHori = ImageUtil.readPPM("test/Model/testImageAscendingColsHoriFlip.ppm");
+
+    assertEquals(imageHori, image);
+  }
+
+}
