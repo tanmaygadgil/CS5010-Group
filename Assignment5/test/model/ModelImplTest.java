3bcd3fbfcfb8b779bc9b0f8f967cf2e2a3a50c7c
Sunny Lee <lees19@wit.edu>
2023-03-29 17:40:40 -0400

    update tests

--- Assignment5/test/model/ModelImplTest.java
+++ Assignment5/test/model/ModelImplTest.java
@@ -12,71 +12,378 @@ import java.util.Arrays;
 import model.filters.GaussianBlur;
 import model.filters.ImageFilter;
 import model.filters.Sharpening;
-import model.loaders.ConventionalImageLoader;
 import model.loaders.PPMImageLoader;
-import model.operations.ImageOperations;
 import model.operations.DitherGreyscaleOperation;
+import model.operations.ImageOperations;
 import model.transforms.GreyscaleTransform;
 import model.transforms.SepiaTransform;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ModelImplTest {
 
+  Model model;
+
+  @Before
+  public void setUp() {
+    this.model = new ModelImpl();
+  }
+
 
   @Test
-  public void load() {
-    InputStream in = null;
-    try {
-      in = new FileInputStream("test/Model/testImage.ppm");
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
-    }
-    try {
-      OutputStream out = new FileOutputStream("test/Model/testOutImage.ppm");
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+  public void testLoad() throws IOException {
+    model.load(new FileInputStream("test/Model/testImage.ppm"), "testImage", "ppm");
+    model.save(new FileOutputStream("test/Model/testSaveImage.ppm"), "testImage", "ppm");
 
-    Model m = new ModelImpl();
-    OutputStream out = null;
-    try {
-      m.load(in, "testImage", "ppm");
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+    int[][][] imageSaved = ImageUtil.readPPM("test/Model/testSaveImage.ppm");
+    int[][][] imageOriginal = ImageUtil.readPPM("test/Model/testImage.ppm");
 
-    //PNG
-    try {
-      in = new FileInputStream("test/Model/greenland_grid_velo.bmp");
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
-    }
-    try {
-      out = new FileOutputStream("test/Model/testOutImage.bmp");
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+    assertEquals(imageOriginal.length, imageSaved.length);
+    assertEquals(imageOriginal[0].length, imageSaved[0].length);
+    assertEquals(imageOriginal[0][0].length, imageSaved[0][0].length);
 
-    try {
-      m.load(in, "testPNGImage", "png");
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
-    }
-    try {
-      m.save(out, "testPNGImage", "png");
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    for (int i = 0; i < imageSaved.length; i++) {
+      for (int j = 0; j < imageSaved[0].length; j++) {
+        for (int k = 0; k < imageSaved[0][0].length; k++) {
+          assertEquals(imageOriginal[i][j][k], imageSaved[i][j][k]);
+        }
+      }
     }
 
   }
 
+  @Test(expected = FileNotFoundException.class)
+  public void testLoadThrowingException() throws FileNotFoundException {
 
+    // the correct path is test/Model/testImage.ppm
+    String path = "test/Model/testImage1.ppm";
+    model.load(new FileInputStream(path), "testImage", "ppm");
+  }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testSaveThrowingException() throws IOException {
+
+    model.load(new FileInputStream("test/Model/testImage.ppm"), "testImage", "ppm");
+    model.save(new FileOutputStream("test/Model/testSaveImage.ppm"), "testImage1", "ppm");
+
+  }
+
+
+  @Test
+  public void testSave() throws IOException {
+    model.load(new FileInputStream("test/Model/testImage.ppm"), "testImage", "ppm");
+    model.save(new FileOutputStream("test/Model/testSaveImage.ppm"), "testImage", "ppm");
+    int[][][] imageSaved = ImageUtil.readPPM("test/Model/testSaveImage.ppm");
+    int[][][] imageOriginal = ImageUtil.readPPM("test/Model/testImage.ppm");
+
+    assertEquals(imageOriginal.length, imageSaved.length);
+    assertEquals(imageOriginal[0].length, imageSaved[0].length);
+    assertEquals(imageOriginal[0][0].length, imageSaved[0][0].length);
+
+    assertEquals(imageOriginal, imageSaved);
+  }
+
+  @Test
+  public void testBrighten() throws IOException {
+    String path = "test/Model/testImage.ppm";
+    int[][][] image = ImageUtil.readPPM(path);
+    int increment = 10;
+
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.brighten(increment, "testImage", "testImageBrighten");
+    model.save(new FileOutputStream("test/Model/testImageBrighten.ppm"), "testImageBrighten",
+        "ppm");
+    int[][][] imageBrighten = ImageUtil.readPPM("test/Model/testImageBrighten.ppm");
+
+    for (int i = 0; i < image.length; i++) {
+      for (int j = 0; j < image[0].length; j++) {
+        for (int k = 0; k < image[0][0].length; k++) {
+          assertEquals(imageBrighten[i][j][k] - image[i][j][k], 10);
+        }
+      }
+    }
+
+  }
+
+  @Test
+  public void testFlipVertical() throws IOException {
+
+    String path = "test/Model/testImageAscendingRows.ppm";
+    int[][][] image = new int[3][3][4];
+    //generating correct answer
+    for (int i = 3; i >= 1; i--) {
+      for (int j = 3; j >= 1; j--) {
+        for (int k = 4; k >= 1; k--) {
+          image[3 - i][3 - j][4 - k] = j;
+        }
+      }
+    }
+
+    model.load(new FileInputStream(path), "testImageAscendingRows", "ppm");
+    model.flip(Axes.VERTICAL, "testImageAscendingRows", "testImageAscendingRowsVertFlip");
+    model.save(new FileOutputStream("test/Model/testImageAscendingRowsVertFlip.ppm"),
+        "testImageAscendingRowsVertFlip", "ppm");
+    int[][][] imageVert = ImageUtil.readPPM("test/Model/testImageAscendingRowsVertFlip.ppm");
+
+    assertEquals(imageVert, image);
+  }
+
+  @Test
+  public void testFlipHorizontal() throws IOException {
+    String path = "test/Model/testImageAscendingCols.ppm";
+    int[][][] image = new int[3][3][4];
+    for (int i = 3; i >= 1; i--) {
+      for (int j = 3; j >= 1; j--) {
+        for (int k = 4; k >= 1; k--) {
+          image[3 - i][3 - j][4 - k] = k;
+        }
+      }
+    }
+
+    model.load(new FileInputStream(path), "testImageAscendingCols", "ppm");
+    model.flip(Axes.HORIZONTAL, "testImageAscendingCols", "testImageAscendingColsHoriFlip");
+    model.save(new FileOutputStream("test/Model/testImageAscendingColsHoriFlip.ppm"),
+        "testImageAscendingColsHoriFlip", "ppm");
+    int[][][] imageHori = ImageUtil.readPPM("test/Model/testImageAscendingColsHoriFlip.ppm");
+
+    assertEquals(imageHori, image);
+  }
+
+
+  @Test
+  public void testImageUtilGreyScale() throws IOException {
+    int[][][] image = new int[1][3][4];
+    for (int i = 0; i < 3; i++) {
+      for (int j = 1; j <= 4; j++) {
+        image[0][i][j - 1] = 4 * i + j;
+      }
+    }
+
+    model.load(new FileInputStream("test/Model/greyImage.ppm"), "greyImage", "ppm");
+    model.save(new FileOutputStream("test/Model/greyImageSave.ppm"), "greyImage", "ppm");
+    int[][][] imageGreyScale = ImageUtil.readPPM("test/Model/greyImageSave.ppm");
+  }
+
+
+  //  @Test (expected = ???.class)
+//  public void testFlipThrowingException() throws ??? {
+  @Test(expected = IllegalArgumentException.class)
+  public void testFlipThrowingException() throws IOException {
+    String path = "test/model/testImage.ppm";
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.flip(Axes.HORIZONTAL, "testImage1", "testImageException");
+  }
+
+
+  //  @Test (expected = ????.class)
+//  public void testGreyscaleThrowingException() throws ??? {
+  @Test(expected = IllegalArgumentException.class)
+  public void testGreyscaleThrowingException() throws IOException {
+
+    String path = "test/Model/testImage.ppm";
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.greyscale(ImageComponents.RED, "testImage1", "testImageException");
+
+  }
+
+
+  @Test
+//  public void testGreyscaleRed() throws ???? {
+  public void testGreyscaleRed() throws IOException {
+
+    String path = "test/Model/testImage.ppm";
+
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.greyscale(ImageComponents.RED, "testImage", "testImageGreyscaledRed");
+    model.save(new FileOutputStream("test/Model/testImageGreyscaledRed.ppm"),
+        "testImageGreyscaledRed", "ppm");
+
+    int[][][] expectedImage = ImageUtil.readPPM("test/Model/expectedImageGreyscaledRed.ppm");
+    int[][][] grayScaledImage = ImageUtil.readPPM("test/Model/testImageGreyscaleRed.ppm");
+    assertEquals(expectedImage, grayScaledImage);
+
+  }
+
+  @Test
+//  public void testGreyscaleGreen() throws ???? {
+  public void testGreyscaleGreen() throws IOException {
+
+    String path = "test/Model/testImage.ppm";
+
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.greyscale(ImageComponents.GREEN, "testImage", "testImageGreyscaledGreen");
+    model.save(new FileOutputStream("test/Model/testImageGreyscaledGreen.ppm"),
+        "testImageGreyscaledGreen", "ppm");
+
+    int[][][] expectedImage = ImageUtil.readPPM("test/Model/expectedImageGreyscaledGreen.ppm");
+    int[][][] grayScaledImage = ImageUtil.readPPM("test/Model/testImageGreyscaleGreen.ppm");
+
+    assertEquals(expectedImage, grayScaledImage);
+
+  }
+
+
+  @Test
+//  public void testGreyscaleBlue() throws ???? {
+  public void testGreyscaleBlue() throws IOException {
+
+    String path = "test/Model/testImage.ppm";
+
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.greyscale(ImageComponents.BLUE, "testImage", "testImageGreyscaledBlue");
+    model.save(new FileOutputStream("test/Model/testImageGreyscaledBlue.ppm"),
+        "testImageGreyscaledBlue", "ppm");
+
+    int[][][] expectedImage = ImageUtil.readPPM("test/Model/expectedImageGreyscaledBlue.ppm");
+    int[][][] grayScaledImage = ImageUtil.readPPM("test/Model/testImageGreyscaleBlue.ppm");
+
+    assertEquals(expectedImage, grayScaledImage);
+
+  }
+
+
+  @Test
+
+//  public void testGreyscaleLuma() throws ???? {
+  public void testGreyscaleLuma() throws IOException {
+
+    String path = "test/Model/testImage.ppm";
+
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.greyscale(ImageComponents.LUMA, "testImage", "testImageGreyscaledLuma");
+    model.save(new FileOutputStream("test/Model/testImageGreyscaledLuma.ppm"),
+        "testImageGreyscaledLuma", "ppm");
+
+    int[][][] expectedImage = ImageUtil.readPPM("test/Model/expectedImageGreyscaledLuma.ppm");
+    int[][][] grayScaledImage = ImageUtil.readPPM("test/Model/testImageGreyscaleLuma.ppm");
+
+    assertEquals(expectedImage, grayScaledImage);
+
+  }
+
+  // Value
+
+  @Test
+//  public void testGreyscaleValue() throws ???? {
+  public void testGreyscaleValue() throws IOException {
+
+    String path = "test/Model/testImage.ppm";
+
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.greyscale(ImageComponents.VALUE, "testImage", "testImageGreyscaledValue");
+    model.save(new FileOutputStream("test/Model/testImageGreyscaledValue.ppm"),
+        "testImageGreyscaledValue", "ppm");
+
+    int[][][] expectedImage = ImageUtil.readPPM("test/Model/expectedImageGreyscaledValue.ppm");
+    int[][][] grayScaledImage = ImageUtil.readPPM("test/Model/testImageGreyscaleValue.ppm");
+
+    assertEquals(expectedImage, grayScaledImage);
+
+  }
+
+  // Intensity
+
+  @Test
+//  public void testGreyscaleIntensity() throws ????? {
+  public void testGreyscaleIntensity() throws IOException {
+
+    String path = "test/Model/testImage.ppm";
+
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.greyscale(ImageComponents.INTENSITY, "testImage", "testImageGreyscaledIntensity");
+    model.save(new FileOutputStream("test/Model/testImageGreyscaledIntensity.ppm"),
+        "testImageGreyscaledIntensity", "ppm");
+
+    int[][][] expectedImage = ImageUtil.readPPM("test/Model/expectedImageGreyscaledIntensity.ppm");
+    int[][][] grayScaledImage = ImageUtil.readPPM("test/Model/testImageGreyscaleIntensity.ppm");
+
+    assertEquals(expectedImage, grayScaledImage);
+
+  }
+
+
+  //  @Test (expected = ???.class)
+//  public void testRgbSplitThrowingException() throws ??? {
+  @Test(expected = IllegalArgumentException.class)
+  public void testRgbSplitThrowingException() throws IOException {
+    String path = "test/Model/testImage.ppm";
+    model.load(new FileInputStream(path), "testImage", "ppm");
+    model.rgbSplit("testImage1", "testImage2", "testImage3", "testImage4");
+
+  }
+
+
+  @Test
+  //public void testRgbSplit() throws ???? {
+  public void testRgbSplit() throws IOException {
+
+    String path = "test/Model/testImage.ppm";
+    model.load(new FileInputStream(path), "testImage", "ppm");
+
+    model.rgbSplit("testImage", "testImageRedSplit", "testImageGreenSplit", "testImageBlueSplit");
+    model.rgbCombine("testImageCombine", "testImageRedSplit", "testImageGreenSplit",
+        "testImageBlueSplit");
+    model.save(new FileOutputStream("test/Model/combinedImage.ppm"), "testImageCombine", "ppm");
+
+    String pathCombinedImage = "test/Model/combinedImage.ppm";
+    int[][][] combinedImage = ImageUtil.readPPM(pathCombinedImage);
+    int[][][] originalImage = ImageUtil.readPPM(path);
+
+    assertEquals(combinedImage, originalImage);
+
+  }
+
+
+  //@Test (expected = ???.class)
+  //public void testRgbCombineThrowingException() throws ??? {
+  @Test(expected = IllegalArgumentException.class)
+  public void testRgbCombineThrowingException() throws IOException {
+
+    String pathRed = "test/Model/testImageRedSplit.ppm";
+    String pathGreen = "test/Model/testImageGreenSplit.ppm";
+    String pathBlue = "test/Model/testImageBlueSplit.ppm";
+
+    model.load(new FileInputStream(pathRed), "ImageRedSplit", "ppm");
+    model.load(new FileInputStream(pathGreen), "ImageGreenSplit", "ppm");
+    model.load(new FileInputStream(pathBlue), "ImageBlueSplit", "ppm");
+
+    model.rgbCombine("testImageCombine", "ImageRedSplit1", "ImageGreenSplit", "ImageBlueSplit");
+    model.rgbCombine("testImageCombine", "ImageRedSplit", "ImageGreenSplit1", "ImageBlueSplit");
+    model.rgbCombine("testImageCombine", "ImageRedSplit", "ImageGreenSplit", "ImageBlueSplit1");
+  }
+
+
+  @Test
+  //public void testRgbCombine() throws ??? {
+  public void testRgbCombine() throws IOException {
+
+    String pathRed = "test/Model/testImageRedSplit.ppm";
+    String pathGreen = "test/Model/testImageGreenSplit.ppm";
+    String pathBlue = "test/Model/testImageBlueSplit.ppm";
+
+    model.load(new FileInputStream(pathRed), "ImageRedSplit", "ppm");
+    model.load(new FileInputStream(pathGreen), "ImageGreenSplit", "ppm");
+    model.load(new FileInputStream(pathBlue), "ImageBlueSplit", "ppm");
+
+    model.rgbCombine("testImageCombine", "ImageRedSplit", "ImageGreenSplit", "ImageBlueSplit");
+    model.save(new FileOutputStream("test/Model/testImageCombine.ppm"), "testImageCombine", "ppm");
+
+    String pathCombinedImage = "test/Model/testImageCombine.ppm";
+    int[][][] combinedImage = ImageUtil.readPPM(pathCombinedImage);
+
+    String pathOriginalImage = "test/Model/testImage.ppm";
+    int[][][] originalImage = ImageUtil.readPPM(pathOriginalImage);
+
+    assertEquals(combinedImage, originalImage);
+
+  }
 
   @Test
   public void testGaussianBlur() throws IOException {
     ImageFilter blur = new GaussianBlur();
 
-    ImageLoader loader = new ConventionalImageLoader();
+    ImageLoader loader = new PPMImageLoader();
     InputStream in = new FileInputStream("test/model/testImage.ppm");
     int[][][] image = loader.load(in);
 
@@ -313,4 +620,6 @@ public class ModelImplTest {
     m.load(in, "testImage", "jpg");
     m.callOperation(dither, "testImage1", "dither");
   }
+
+
 }
